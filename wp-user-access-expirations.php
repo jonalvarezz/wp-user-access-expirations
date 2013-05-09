ea558a06f09663cf1c93456da5eb93a5fba7f8be
Jona <jonas.al.co@gmail.com>
2013-05-09 14:06:51 -0500

    removing dev control

--- wp-user-access-expirations.php
+++ wp-user-access-expirations.php
@@ -134,17 +134,12 @@ class UserAccessExpiration
 		$user_notified = array();
 		$user_notified_fail = array();
 		foreach ( $users as $user ) {
-			if( $user->user_email == 'soporte@invertirmejor.com') {
-					if( wp_mail( $user->user_email, $subject, $message, $headers ) ) {
-					update_user_meta( $user->ID, self::user_meta_expire_count, '1' );
-					$user_notified[] = $user->user_email;
-				}
-				else {
-					$user_notified_fail[] = $user->user_email;
-				}
+			if( wp_mail( $user->user_email, $subject, $message, $headers ) ) {
+				update_user_meta( $user->ID, self::user_meta_expire_count, '1' );
+				$user_notified[] = $user->user_email;
 			}
 			else {
-				$user_notified[] = $user->user_email;
+				$user_notified_fail[] = $user->user_email;
 			}
 		}
 
